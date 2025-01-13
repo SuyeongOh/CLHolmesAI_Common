ee95b414f7978b923a9b02919010df7aca18dcfb
Dae-Yeol Kim <spicyyeol@gmail.com>
2025-01-13 19:24:02 +0900

    feat : add the comment

--- test/test_beat_analysis.py
+++ test/test_beat_analysis.py
@@ -12,29 +12,34 @@ result_class = []
 
 
 def run(np_data: dict):
-    # pid별로 QRS
-    pid_list = np.unique(np_data['pid'])
-    pid_beat_ppv = {}
-    pid_beat_sense = {}
-    pid_beat_fn = {}
+    # 각 환자(pid) 별로 QRS 비트 분석을 수행
+    pid_list = np.unique(np_data['pid'])    # 고유 pid 추출
+    pid_beat_ppv = {}                       # PPV 저장
+    pid_beat_sense = {}                     # Sensitivity 저장
+    pid_beat_fn = {}                        # FN 저장
+
+    # 분류 모델 로드
     beat_model = BeatClassifierArchitecture().model
     beat_model.load_weights("weights/heartbeat_classifier_weights.h5")
 
     for pid in pid_list:
+        # 특정 환자(pid)의 데이터 인덱스 추출
         pid_idxs = np.where(np_data['pid'] == pid)[0]
         onset = pid_idxs[0]
         offset = pid_idxs[-1]
-
+        # 해당 pid의 데이터 및 레이블 추출
         target_data = np_data['data'][onset:offset]
         target_label = np_data['group'][onset:offset]
         for beat in target_data:
+            # 비트를 리샘플링 및 전처리
             resample_beat = np.array(resample(beat, 125))
             preprocessed_beat = min_max_normalization(nk.signal_detrend(resample_beat))
             resample_signal.append(preprocessed_beat)
 
-
+        # 리샘플링된 신호를 모델 입력 형식에 맞게 변환
         resample_np_array = np.concatenate([arr[np.newaxis, :, np.newaxis] for arr in resample_signal], axis=0)
 
+        # 비트 클래스 맵 정의
         # compare result_class/np_data['group']
         # a number of F = (V + A) - for holmesai
         # original map -> beat_class_map = {0: 'N', 1: 'S', 2: 'V', 3: 'A'}
@@ -48,10 +53,12 @@ def run(np_data: dict):
         beat_class_per_record = {}
         # Beat Analysis Model
 
+        # 모델을 통해 분류 결과 예측
         result_class = beat_model.predict(resample_np_array, batch_size=BATCH_SIZE)
         result_class = np.argmax(result_class, axis=1)
 
         for cls, label in zip(result_class, target_label):
+            # 예측 결과와 실제 레이블 비교
             if beat_class_map[cls] == label:
                 beat_class_correct[label].append(label)
             else:
@@ -64,6 +71,8 @@ def run(np_data: dict):
                     print(f'create new dict {label}_{beat_class_map[cls]}')
                     beat_class_incorrect[f'{label}_{beat_class_map[cls]}'] = []
                     beat_class_incorrect[f'{label}_{beat_class_map[cls]}'].append(cls)
+
+        # Sensitivity 계산
         total_class_count = {'N': 0, 'V': 0, 'S': 0}
         total_class_count['N'] = np.sum(np_data['group'] == 'N')
         total_class_count['V'] = np.sum(np_data['group'] == 'V')
@@ -78,33 +87,19 @@ def run(np_data: dict):
             elif cls == 1 or cls == 3:
                 total_analysis_class_count['S'] += 1
 
-        if total_class_count['N'] != 0:
-            n_sense = len(beat_class_correct['N']) / total_class_count['N']
-        else:
-            n_sense = 0
-        if total_class_count['V'] != 0:
-            v_sense = len(beat_class_correct['V']) / total_class_count['V']
-        else:
-            v_sense = 0
-        if total_class_count['S'] != 0:
-            s_sense = len(beat_class_correct['S']) / total_class_count['S']
-        else:
-            s_sense = 0
+        # Sensitivity 및 PPV 계산
+        n_sense = len(beat_class_correct['N']) / total_class_count['N'] if total_class_count['N'] != 0 else 0
+        v_sense = len(beat_class_correct['V']) / total_class_count['V'] if total_class_count['V'] != 0 else 0
+        s_sense = len(beat_class_correct['S']) / total_class_count['S'] if total_class_count['S'] != 0 else 0
 
+        n_ppv = len(beat_class_correct['N']) / total_analysis_class_count['N'] if total_analysis_class_count[
+                                                                                      'N'] != 0 else 0
+        v_ppv = len(beat_class_correct['V']) / total_analysis_class_count['V'] if total_analysis_class_count[
+                                                                                      'V'] != 0 else 0
+        s_ppv = len(beat_class_correct['S']) / total_analysis_class_count['S'] if total_analysis_class_count[
+                                                                                      'S'] != 0 else 0
 
-        if total_analysis_class_count['N'] != 0:
-            n_ppv = len(beat_class_correct['N']) / total_analysis_class_count['N']
-        else:
-            n_ppv = 0
-        if total_analysis_class_count['V'] != 0:
-            v_ppv = len(beat_class_correct['V']) / total_analysis_class_count['V']
-        else:
-            v_ppv = 0
-        if total_analysis_class_count['S'] != 0:
-            s_ppv = len(beat_class_correct['S']) / total_analysis_class_count['S']
-        else:
-            s_ppv = 0
-
+        # False Negative 계산
         v_fn = len(beat_class_fp['V']) / (len(beat_class_tn['V']) + len(beat_class_fp['V']))
         s_fn = len(beat_class_fp['S']) / (len(beat_class_tn['S']) + len(beat_class_fp['S']))
 
@@ -126,8 +121,9 @@ def run(np_data: dict):
         #         pid_count = pid_list.count(pid)
         #         print(f'incorrect - class, pid : ({cls}, {pid}, {pid_count})')
 
+        # 각 pid 별 결과 저장
         pid_beat_ppv[pid] = v_ppv
         pid_beat_sense[pid] = v_sense
         pid_beat_fn[pid] = v_fn
-
+        # 리샘플링 신호 초기화
         resample_signal.clear()
